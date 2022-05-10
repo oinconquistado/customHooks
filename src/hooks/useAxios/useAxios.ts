2edf92f9af6010f0bcfb1a4d740f6740c15ee71e
Neto <ss.neto88@gmail.com>
2022-05-10 16:08:00 -0300

    fix: useAxios Correction

--- src/hooks/useAxios/useAxios.ts
+++ src/hooks/useAxios/useAxios.ts
@@ -1,60 +1,59 @@
-import { useCallback, useEffect } from 'react';
-import axios, { AxiosRequestConfig } from 'axios';
+import { useCallback } from 'react';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { atom, useAtom } from 'jotai';
 
-let axiosResponse: any;
+let axiosResponse: AxiosResponse;
 
 interface axiosRequestTypes extends AxiosRequestConfig {
-  delay?: number;
   body?: any;
   headers?: any;
 }
 
-const dataAtom = atom<{} | undefined>({});
+const dataAtom = atom([]);
 const loadingAtom = atom({});
 const erroAtom = atom({});
 
-const useAxios = ({ url, method, params, body = null, headers = null }: axiosRequestTypes) => {
+const useAxios = () => {
   const [dataResponse, setDataResponse] = useAtom(dataAtom);
   const [loading, setLoading] = useAtom(loadingAtom);
   const [erro, setErro] = useAtom(erroAtom);
 
-  const fetchData = useCallback(async () => {
-    try {
-      axiosResponse = await axios({
-        url: url,
-        method: method,
-        params: params,
-        data: body,
-        headers: headers,
-        baseURL: `https://bus-iot.herokuapp.com/`,
-        onUploadProgress: () => {
-          setLoading(true);
-        },
-      });
-    } catch (error: any) {
-      if (error.response) {
-        setDataResponse(undefined);
-        setErro(error);
-      } else if (error.request) {
-        console.log(error.request);
-      } else {
-        console.log('Error', error.message);
+  const fetchData = useCallback(
+    async ({ url, method, params, body = null, headers = null }: axiosRequestTypes) => {
+      try {
+        axiosResponse = await axios({
+          url: url,
+          method: method,
+          params: params,
+          data: body,
+          headers: headers,
+          baseURL: `https://bus-iot.herokuapp.com/`,
+          onUploadProgress: () => {
+            setLoading(true);
+          },
+        });
+      } catch (error: any) {
+        if (error.response) {
+          setDataResponse([]);
+          setErro(error);
+        } else if (error.request) {
+          console.log(error.request);
+        } else {
+          console.log('Error', error.message);
+        }
+      } finally {
+        if (axiosResponse && axiosResponse.status === 200) {
+          setDataResponse(axiosResponse.data);
+          setLoading(false);
+        }
       }
-    } finally {
-      if (axiosResponse.status === 200) {
-        setDataResponse(axiosResponse.data);
-        setLoading(false);
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-  }, [method, url, body, headers, fetchData]);
+    },
+    [setDataResponse, setErro, setLoading]
+  );
 
   return {
     axiosRequest: {
+      fetchData,
       dataResponse,
       loading,
       erro,
