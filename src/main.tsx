ee92f67a8263d3a288e9a08e8a52c0d63dcce7cc
Neto <ss.neto88@gmail.com>
2022-06-26 09:39:51 -0300

    fix: removed Strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Provider } from 'jotai';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "jotai";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
