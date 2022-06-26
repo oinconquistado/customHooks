c0e0973262daf4e7378bf183218a9bfa1086b57f
Neto <ss.neto88@gmail.com>
2022-06-26 15:45:47 -0300

    feature(main): page of hooks cards 🃏

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import { useEffect, useState } from 'react';
-import useAxios from './hooks/useAxios/useAxios';
+import React from "react";
+import "./Reset.css";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHammer } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Playground</h1>
+      <header>
+        <FontAwesomeIcon fixedWidth icon={faHammer} size='2x' style={{ margin: 5 }} color='#313131' />
+        <h1 className='headerTitle'>customHooks</h1>
+        <p>I created a test playground for each hook. ;)</p>
+      </header>
+      <div className='container'>
+        <div className='hooksCard blueGreen '>
+          <div className='overlay'>
+            <p className='hooksDescriptions'>Make requests using axios</p>
+          </div>
+          <h2 className='hookTitle'>useAxios</h2>
+        </div>
+        <div className='hooksCard marineBlue'>
+          <div className='overlay'>
+            <p className='hooksDescriptions'>Make requests using axios</p>
+          </div>
+          <h2 className='hookTitle'>UseStorage</h2>
+        </div>
+        <div className='hooksCard pinkRed'>
+          <div className='overlay'>
+            <p className='hooksDescriptions'>Make requests using axios</p>
+          </div>
+          <h2 className='hookTitle'>useDelay</h2>
+        </div>
+      </div>
     </div>
   );
 }
